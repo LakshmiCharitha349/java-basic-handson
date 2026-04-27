276a13b1ca5e4eec591b2a36f0cb0ec5f9a29824
Marthala Charitha <marthalacharitha@Marthalas-MacBook-Pro.local>
2026-04-27 15:07:06 +0530

    Refactor Money class and comment out StringUtils test calls
    - Implement Money class with value field and basic arithmetic methods (add, minus)
    - Add constructor and encapsulate currency operations
    - Keep containsChar test active for validation

--- Money.java
+++ Money.java
@@ -1,1 +1,20 @@
 // Money.java
+
+class Money {
+final int value;
+
+   Money(int amount) {
+     this.value = amount;
+  }
+
+  Money add(Money rupees) {
+    int total = this.value + rupees.value;
+    return new Money(total);
+
+  }
+
+  Money minus(Money rupees) {
+    int total = this.value - rupees.value;
+    return new Money(total);
+  }
+}
